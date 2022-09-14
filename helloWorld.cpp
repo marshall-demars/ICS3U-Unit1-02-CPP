1f82f824c328accb5c4ec09294b4b149e57c98ec
Marshall Demars <marshall.demars@mths.ca>
2022-09-14 15:02:18 +0000

    changed comment

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,10 +1,10 @@
-// Copyright 2022 Marshall DeMars
+// Copyright 2022 Marshall Demars
 
 #include <iostream>
 
 int main() {
     std::cout << "Hello, World!\n";
-    std::cout << "Goodbye,, World!\n";
+    std::cout << "Goodbye, World!\n";
 
     std::cout << "\nDone.";
 }
